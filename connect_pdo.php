bd627f83d6d3afd8c589e382403faeab9c08be2f
Brad Francis <s3385315@student.rmit.edu.au>
2015-06-05 07:58:02 +0000

    Fixed PDO connection string

--- connect_pdo.php
+++ connect_pdo.php
@@ -7,7 +7,7 @@
   require_once('db.php');
   
   try {
-    $dbh = new DBO('mysql:host=$DB_HOST;dbname=$DB_NAME', DB_USER, DB_PW);
+    $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PW);
     
     echo "<p>Connected to " . DB_NAME . "<br />\n";
   } catch (PDOException $e) {
