bdb40c4446bfd9409b14f355e2be36d8d0795cc1
Brad Francis <s3385315@student.rmit.edu.au>
2015-06-06 19:17:17 +1000

    Update connect_pdo.php

--- connect_pdo.php
+++ connect_pdo.php
@@ -7,7 +7,7 @@
   require_once('db.php');
   
   try {
-    $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PW);
+    $dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PW);
   } catch (PDOException $e) {
     echo "<p>ERROR: " . $e->getMessage() . "<br />\n";
     die();
