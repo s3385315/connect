1049e76d24009b6070d816dfc42aac8db7a3bd1d
Brad Francis <s3385315@student.rmit.edu.au>
2015-06-10 01:52:15 +0000

    Placed connection code inside function

--- connect_pdo.php
+++ connect_pdo.php
@@ -1,18 +1,13 @@
-<html DOCTYPE>
-<head><title>Connect with PDO</title></head>
-
-<body>
-
 <?php
   require_once('db.php');
-  
-  try {
-    $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PW);
-  } catch (PDOException $e) {
-    echo "<p>ERROR: " . $e->getMessage() . "<br />\n";
-    die();
-  }
-?>
 
-</body>
-</html>
+    function connect_to_db() {
+        try {
+            return new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PW);
+        } catch (PDOException $e) {
+            echo "<p>ERROR: " . $e->getMessage() . "<br />\n";
+            return null;
+        }
+    }
+
+
